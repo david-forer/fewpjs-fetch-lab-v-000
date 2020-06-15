371fdc6297b523b14171d026c10086d69f4badff
David Forer <david.j.forer@gmail.com>
2020-06-15 08:49:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ function renderBooks(json) {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
-     const h4 = document.createElement('h4')
-    h4.innerHTML = `<h4>${book.author}</h4>`
-    main.appendChild(h2)
+   
   })
 }
 
